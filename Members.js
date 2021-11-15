832d523e36a68483c3a36c576a1773a533059e99
anonymousdomain <domainanonymous1@gmail.com>
2021-11-15 21:44:25 +0300

    init middleware

---
+++ Members.js
@@ -0,0 +1,21 @@
+ const members = [
+    { 
+        id:1,
+        name:"dawit",
+        email:"devo@gmail.com",
+        status:"active"
+    },
+    { 
+        id:2,
+        name:"kal",
+        email:"kal@gmail.com",
+        status:"active"
+    },
+    { 
+        id:3,
+        name:"nahom",
+        email:"nahom@gmail.com",
+        status:"inactive"
+    },
+];
+module.exports=members
--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const members=require('./Members')
 //init express
 const app = express();
 
+const log=(req,res,next)=>{
+    console.log('init')
+    next()
+}
+
+app.use(log);
 app.get('/api/members', (req, res) =>res.json(members))
 
 const Port = process.env.PORT || 5000
