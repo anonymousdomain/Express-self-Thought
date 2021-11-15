3fd84e3a396f754a8ffeff7139e98d6bd7037e65
anonymousdomain <domainanonymous1@gmail.com>
2021-11-15 21:16:18 +0300

    efficent way to serve static pages from public

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const path =require('path')
 //init express
 const app=express();
 
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
-app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'))
-})
-
+app.use(express.static(path.join(__dirname,'public')))
 const Port=process.env.PORT||5000
 
 app.listen(Port,()=>{
