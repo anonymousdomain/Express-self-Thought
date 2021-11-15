aed86b660350095c3493a3c0cff7f77f8308ab87
anonymousdomain <domainanonymous1@gmail.com>
2021-11-15 21:40:13 +0300

    put json object in a separet file

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 const express = require("express")
 const path = require('path')
+const members=require('./Members')
 //init express
 const app = express();
-members = [
-    { 
-        id:1,
-        name:"dawit",
-        email:"devo@gmail.com",
-        status:"active"
-    },
-    { 
-        id:2,
-        name:"kal",
-        email:"kal@gmail.com",
-        status:"active"
-    },
-    { 
-        id:3,
-        name:"nahom",
-        email:"nahom@gmail.com",
-        status:"inactive"
-    },
-];
+
 app.get('/api/members', (req, res) =>res.json(members))
 
 const Port = process.env.PORT || 5000
