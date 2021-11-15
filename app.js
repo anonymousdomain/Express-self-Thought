bb273298f6ec2594c3e07deced1996847d645a8b
anonymousdomain <domainanonymous1@gmail.com>
2021-11-15 21:06:53 +0300

    loading html file from a public folder

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const app=require("express")()
-
+const express=require("express")
+const path =require('path')
 //init express
-//const app=express();
+const app=express();
 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
 const Port=process.env.PORT||5000
