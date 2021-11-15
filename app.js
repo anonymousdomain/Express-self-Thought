96f8ce016085473e89b068d6c94f63e2b5e91c75
anonymousdomain <domainanonymous1@gmail.com>
2021-11-15 21:27:30 +0300

    simple api fetching hardcoded json data

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express=require("express")
-const path =require('path')
+const express = require("express")
+const path = require('path')
 //init express
-const app=express();
+const app = express();
+members = [
+    { 
+        id:1,
+        name:"dawit",
+        email:"devo@gmail.com",
+        status:"active"
+    },
+    { 
+        id:2,
+        name:"kal",
+        email:"kal@gmail.com",
+        status:"active"
+    },
+    { 
+        id:3,
+        name:"nahom",
+        email:"nahom@gmail.com",
+        status:"inactive"
+    },
+];
+app.get('/api/members', (req, res) =>res.json(members))
 
-app.use(express.static(path.join(__dirname,'public')))
-const Port=process.env.PORT||5000
+const Port = process.env.PORT || 5000
 
-app.listen(Port,()=>{
+app.listen(Port, () => {
     console.log(`server running on port ${Port}`)
 })
