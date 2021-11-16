375379ae56f631058fccaa8f61f493fc2f6278bd
anonymousdomain <domainanonymous1@gmail.com>
2021-11-17 00:07:45 +0300

    controllers

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,65 +1,19 @@
 import express from 'express';
+import {getMembers,createMembers,putMembers,deleteMember} from "../../controllers/controller.js"
 
-import members from '../../Members.js';
-import {v4 as uuid} from "uuid"
 
 const router = express.Router();
-
+//get request
 router.get('/', (req, res) => res.json(members))
 
-router.get('/:id', (req, res) => {
-    const idFound = members.some(member => member.id === parseInt(req.params.id));
-    if (idFound) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({msg:`memebr with the id of ${req.params.id} is not found`})
-    }
+router.get('/:id',getMembers)
 
-})
 //create a new member 
-router.post('/', (req, res) => {
-   
-    const member=req.body;
-
-    const found = members.some(member => member.email === req.body.email);
-    if (!req.body.name || !req.body.email) {
-        return res.status(400).json({ msg: "pelase enter the required information" })
-    } if (found) {
-        return res.status(400).json({ msg: "the user already exist" })
-    }
-    members.push({id,...member});
-
-   //res.json(members)
-   res.redirect('/')
-})
+router.post('/',createMembers)
 
 //update member
-
-router.put('/:id',(req,res)=>{
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found) {
-        const updateMember=req.body;
-        members.forEach(member=>{
-            if(member.id===parseInt(req.params.id)){
-                member.name=updateMember.name?updateMember:member.name
-                member.email=updateMember.email?updateMember:member.email
-            res.json({msg:'member updated',member})
-            }
-        })
-    } else {
-        res.status(400).json({msg:`memebr with the id of ${req.params.id} is not found`})
-    }
-})
+router.put('/:id',putMembers)
 //delete
-
-router.delete('/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found) {
-       res.json({members:members.filter(member => member.id !== parseInt(req.params.id)),msg:`deletd user:${req.params.id}`});
-    } else {
-      res.status(400).json({msg:`memebr with the id of ${req.params.id} is not found`})
-    }
-
-})
+router.delete('/:id', deleteMember)
 export default router;
 
