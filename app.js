78f278b567168e3110b74de18dfe6b0cd05aa178
anonymousdomain <domainanonymous1@gmail.com>
2021-11-16 10:27:14 +0300

    rendering memebrs

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express")
-const {engine}=require('express-handlebars')
+const { engine } = require('express-handlebars')
 const path = require('path')
+const members=require('./Members')
 const log = require('./Middleware')
 //init express
 const app = express();
 //app.use(log);
 
 //Handlebars middleware
- app.engine('handlebars',engine())
- app.set('view engine','handlebars')
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
- app.set('views','./views')
-  app.get('/',(req,res)=>res.render('index'))
+app.set('views', './views')
+
+app.get('/', (req, res) => res.render('index',{
+    appTitle:"devo Member app",
+    members:members
+}))
 //body parser meddleware
 app.use(express.json());
 
